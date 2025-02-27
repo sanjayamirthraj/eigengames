c42f395aeb6257a01a6d2cbbd3c1e3dc6a519cbd
souradeep-das <dsouradeep2@gmail.com>
2025-02-27 03:55:36 -0700

    spin parallel exec helper as a part of the local stack

--- AVS/Othentic-AVS/Execution_Service/src/parallelBlock.service.js
+++ AVS/Othentic-AVS/Execution_Service/src/parallelBlock.service.js
@@ -4,8 +4,8 @@ const axios = require("axios");
 async function getBlock() {
     try {
         // const result = await axios.get('http://localhost:3000/blocks');
-        // the helper under AVS/parallel-exec-helper is deployed on render at this URL
-        const result = await axios.get('https://parallel-exec-helper.onrender.com/blocks');
+        // the helper under AVS/parallel-exec-helper is running on docker container
+        const result = await axios.get('http://10.8.0.102:3000/blocks');
         return result.data.blocks;
 
     } catch (err) {
